bd21179e77130048f42c67481d23524df755b195
kyamisuke <murakami.k.am@m.titech.ac.jp>
2024-05-06 21:56:43 +0900

    ボタンのアクティブ切り替え

---
+++ nexthouselab-judge/nexthouselab-judge/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  nexthouselab-judge
+//
+//  Created by 村上航輔 on 2024/05/06.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    let judgeName: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView(judgeName: "KAZANE")
+}
---
+++ nexthouselab-judge/nexthouselab-judge/nexthouselab_judgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexthouselab_judgeApp.swift
+//  nexthouselab-judge
+//
+//  Created by 村上航輔 on 2024/05/06.
+//
+
+import SwiftUI
+
+@main
+struct nexthouselab_judgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
