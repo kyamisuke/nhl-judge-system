6047c8c57bc7795e88ed007a383a0c93bb4707d7
kyamisuke <murakami.k.am@m.titech.ac.jp>
2024-05-08 08:54:37 +0900

    ホスト側のプロジェクトを作成

---
+++ nexthouselab-host/nexthouselab-host/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nexthouselab-host
+//
+//  Created by 村上航輔 on 2024/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nexthouselab-host/nexthouselab-host/nexthouselab_hostApp.swift
@@ -0,0 +1,17 @@
+//
+//  nexthouselab_hostApp.swift
+//  nexthouselab-host
+//
+//  Created by 村上航輔 on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct nexthouselab_hostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
