f042aadb08bacd1a2c60d0d0b2b2eaaf7b6baa5f
kyamisuke <murakami.k.am@m.titech.ac.jp>
2024-05-08 09:25:10 +0900

    ファイルIO処理を追加

---
+++ nexthouselab-host/nexthouselab-host/Ex/DocumentPickerView.swift
@@ -0,0 +1,52 @@
+//
+//  DocumentPickerView.swift
+//  nexthouselab-host
+//
+//  Created by 村上航輔 on 2024/05/08.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+ 
+struct DocumentPickerView : UIViewControllerRepresentable {
+    let openingContentTypes: [UTType]
+    let asCopy: Bool
+ 
+    private var didPickDocumentCallback: ((URL) -> Void)?
+ 
+    init(openingContentTypes: [UTType], asCopy: Bool = false) {
+        self.openingContentTypes = openingContentTypes
+        self.asCopy = asCopy
+    }
+ 
+    func makeUIViewController(context: Context) -> UIDocumentPickerViewController {
+        let documentPickerViewController =  UIDocumentPickerViewController(forOpeningContentTypes: openingContentTypes, asCopy: asCopy)
+        documentPickerViewController.delegate = context.coordinator
+        return documentPickerViewController
+    }
+ 
+    func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: Context) {}
+ 
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+ 
+    /// ドキュメントが選択された際のコールバックを指定します
+    func didPickDocument(callback: @escaping (URL) -> Void) -> Self {
+        var view = self
+        view.didPickDocumentCallback = callback
+        return view
+    }
+ 
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        var parent: DocumentPickerView
+ 
+        init(_ parent: DocumentPickerView) {
+            self.parent = parent
+        }
+ 
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
+            self.parent.didPickDocumentCallback?(url)
+        }
+    }
+}
---
+++ nexthouselab-host/nexthouselab-host/View/Component/UserDictionaryView.swift
@@ -0,0 +1,113 @@
+//
+//  UserDictionaryView.swift
+//  nexthouselab-host
+//
+//  Created by 村上航輔 on 2024/05/08.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+ 
+struct FolderImportView: View {
+    @State private var showsImportDocumentPicker = false
+    @State private var fileName = ""
+    @Binding var fileContent: String
+ 
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text(fileName)
+                Button("importFromFile", action: {
+                    showsImportDocumentPicker = true
+                })
+            }
+        }
+        .sheet(isPresented: $showsImportDocumentPicker) {
+            // 2. ユーザにファイルを選択させ、その内容を読み取る。
+            DocumentPickerView(openingContentTypes: [UTType.text])
+            .didPickDocument { fileURL in
+                do {
+                    guard fileURL.startAccessingSecurityScopedResource() else { return }
+                    defer { fileURL.stopAccessingSecurityScopedResource() }
+                    fileName = (fileURL.path() as NSString).lastPathComponent
+                    let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+                    print(fileContent)
+                    self.fileContent = fileContent
+                } catch {
+                    print("Failed to import")
+                }
+            }
+        }
+    }
+}
+
+struct DemoFolderExportView: View {
+    @State private var showsExportDocumentPicker = false
+ 
+    var body: some View {
+        VStack(spacing: 0) {
+            Button("exportToFile", action: {
+                showsExportDocumentPicker = true
+            })
+        }
+        .sheet(isPresented: $showsExportDocumentPicker) {
+            // 1. ユーザにフォルダーを選択させ、そこにファイルを作成する。
+            DocumentPickerView(openingContentTypes: [UTType.folder])
+                .didPickDocument { directoryURL in
+                    guard directoryURL.startAccessingSecurityScopedResource() else { return }
+                    defer { directoryURL.stopAccessingSecurityScopedResource() }
+                    let newFileURL = directoryURL.appendingPathComponent("fileName.tsv")
+                    do {
+                        try "kyami,kenshu,amazon".write(to: newFileURL, atomically: true, encoding: .utf8)
+                    } catch {
+                        print("Failed to export")
+                    }
+                }
+        }
+    }
+}
+
+struct FolderExportView: View {
+    @State private var showsExportDocumentPicker = false
+    @Binding var scores: [Float]
+    let fileName: String
+ 
+    var body: some View {
+        VStack(spacing: 0) {
+            Button("exportToFile", action: {
+                showsExportDocumentPicker = true
+            })
+        }
+        .sheet(isPresented: $showsExportDocumentPicker) {
+            // 1. ユーザにフォルダーを選択させ、そこにファイルを作成する。
+            DocumentPickerView(openingContentTypes: [UTType.folder])
+                .didPickDocument { directoryURL in
+                    guard directoryURL.startAccessingSecurityScopedResource() else { return }
+                    defer { directoryURL.stopAccessingSecurityScopedResource() }
+                    let newFileURL = directoryURL.appendingPathComponent("\(fileName).csv")
+                    do {
+                        var data = ""
+                        for score in scores {
+                            data += String(score) + ","
+                        }
+                        try data.write(to: newFileURL, atomically: true, encoding: .utf8)
+                    } catch {
+                        print("Failed to export")
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    struct PreviewView: View {
+        @State var fileContent: String = ""
+        
+        var body: some View {
+            FolderImportView(fileContent: $fileContent)
+            DemoFolderExportView()
+        }
+    }
+    
+    return PreviewView()
+}
